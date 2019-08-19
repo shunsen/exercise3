889608f53e567d36f0ff8bc18663747681e233ae
zss <1740308215@qq.com>
2019-08-19 16:00:26 +0800

    fixed test item for async request

--- test/test.js
+++ test/test.js
@@ -35,8 +35,9 @@ describe('jQuery', function () {
 
   it('should able to request https://raw.githubusercontent.com/FE-star/exercise1/master/test/test.js', function (done) {
     // 使用 jQuery.ajax 请求 https://raw.githubusercontent.com/FE-star/exercise1/master/test/test.js，并验证是否拿到文件
-    jQuery.ajax({
-      url:'https://raw.githubusercontent.com/FE-star/exercise1/master/test/test.js'
+    jQuery.get({
+      url:'https://raw.githubusercontent.com/FE-star/exercise1/master/test/test.js',
+      type:'get'
     }).done(function(data){
       if(!!data) done()
     })
